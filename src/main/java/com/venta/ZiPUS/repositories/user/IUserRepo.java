8943329dff587c43adef9733e7cc85466bfa0176
reinis-sim <reinissimsonsrs@gmail.com>
2020-06-20 21:39:49 +0300

    Izveidots IUserRepo

---
+++ src/main/java/com/venta/ZiPUS/repositories/user/IUserRepo.java
@@ -0,0 +1,10 @@
+package com.venta.ZiPUS.repositories.user;
+
+import com.venta.ZiPUS.models.User.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
+
+public interface IUserRepo extends CrudRepository<User, Long> {
+    ArrayList<User> findAll();
+}
